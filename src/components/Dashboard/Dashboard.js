ebc262014c0d87175bbfb3a53d990e5019c555f3
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-08-02 17:54:31 +0545

    Display email in Dashboard Component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -12,11 +12,11 @@ function Dashboard(props) {
     else {
       props.history.push("/login");
     }
-  }, []);
+  }, [props]);
 
   return (
     <div>
-      
+      <h1>{user.email}</h1>
     </div>
   )
 }
