30ddabf1ded9bdd30147e636ad6c167fe1028e4d
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-08-03 17:21:49 +0545

    timeConverter update logic

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -49,9 +49,6 @@ function Task({ user }) {
     let year = a.getFullYear();
     let month = months[a.getMonth()];
     let date = a.getDate();
-    let hour = a.getHours();
-    let min = a.getMinutes();
-    let sec = a.getSeconds();
     let time = date + ' ' + month + ' ' + year;
     return time;
   }
